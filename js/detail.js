a01ce2aafacf19bf1ddd527fbd5eb0b5ec995fa8
Be Mai <bemai@gmail.com>
2022-11-24 15:58:48 +0700

    background

--- js/detail.js
+++ js/detail.js
@@ -3,8 +3,6 @@ let model = document.querySelector('.model-podcast')
 let close = document.querySelector('.close')
 let back = document.querySelector('.back-mobile')
 
-let btntop = document.querySelector('.gg-arrow-long-up')
-
 btn.addEventListener('click', function () {
   model.style.display = 'block'
   model.style.overflowY = 'scroll'
@@ -21,8 +19,4 @@ close.addEventListener('click', function () {
 back.addEventListener('click', function () {
   model.style.display = 'none'
   document.body.style.overflowY = 'initial'
-})
-
-btntop.addEventListener('click', function () {
-  console.log('object')
 })
